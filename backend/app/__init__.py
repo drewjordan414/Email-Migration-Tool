8088346f3c984baf1d370fde0f87ca3e5ed09489
Drew Jordan <andrewrjordan404@gmail.com>
2025-01-18 15:04:39 -0600

    new

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_pyfile('../.env')
     
-    # Import and register blueprints here
+    # Load environment variables from the .env file
+    load_dotenv(dotenv_path='../.env')
+    
+    # Set configuration variables
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret_key')  # Default as fallback
+    app.config['DEBUG'] = os.getenv('DEBUG', 'False').lower() == 'true'  # Convert to boolean
+    
+    # Import and register blueprints
     from .routes import main
     app.register_blueprint(main)
 
